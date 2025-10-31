77a774216590f3c089ed3e226dfe1fbc1ddd3d96
“Komisch-cake3” <madison.o.mayer@gmail.com>
2025-10-31 11:51:39 -0500

    I used javascript to make it change through the images as you click.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const cookie =
+const cookie = document.getElementById("cookie");
+
+const cookieImages = [
+    "Images/main.png",
+    "Images/cookie1.png",
+    "Images/cookie2.png",
+    "Images/cookie3.png",
+    "Images/cookie4.png",
+    "Images/cookie5.png",
+    "Images/cookie6.png"
+];
+
+let currentIndex = 0;
+cookie.addEventListener("click", () => {
+    console.log("cookie clicks!");
+    currentIndex++;
+    if (currentIndex >= cookieImages.length) {
+        currentIndex = 0;
+    
+    }
+       
+    console.log("switching to:", cookieImages[currentIndex]);
+     cookie.src = cookieImages[currentIndex];
+
+});
